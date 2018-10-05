5b679269cd4a4eaadd4d2e5bdc7d667a2df1b20f
DDarrow123 <ddarrowphl@gmail.com>
2018-10-05 11:19:21 -0400

    Module 1 Code Challenge - round 2

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,20 @@
+require 'pry'
+
 class QueueItem
 
+  attr_accessor :viewer, :movie, :rating
+
+  @@all = []
+
+  def initialize(viewer, movie, rating)
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,39 @@
+require 'pry'
+
 class Viewer
   attr_accessor :username
 
   @@all = []
 
-  def initialize(username)
-    @username = username
-    self.class.all << self
+    def initialize(username)
+      @username = username
+      self.class.all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def queue_items
+      QueueItem.all.select do |item|
+        item.viewer == self
+      end
+    end
+
+    def queue_movies
+      QueueItem.all.select do |item|
+        item.movie == self
+        # binding.pry
+    end
   end
 
-  def self.all
-    @@all
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
   end
-  
+
+#did not have time to finish this method
+  def rate_movie(movie, rating)
+    QueueItem.new(self, movie, rating)
+  end
+
 end
